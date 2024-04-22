c1f66ac005d79ff8a0b8ad606dda710fc8ebf411
Douniabf <157621126+Douniabf@users.noreply.github.com>
2024-04-22 19:37:17 +0200

    Mise à jour 2

--- assets/script.js
+++ assets/script.js
@@ -10,9 +10,11 @@ const slides = [
 	{
 		"image":"slide3.jpg",
 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+	},
+	{
+		"image":"slide4.png",
+		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
-
-
 ]
 
 // Variables
